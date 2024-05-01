3af5bade16e7ff09cb4019d0b8b06f0e05e4817b
Rahul Rathor <www.rahulr41180@gmail.com>
2024-05-01 21:05:52 +0530

    Redux Code Funtionality Update

---
+++ Redux/Course-1/3rd_React-Redux/1stclient/src/Reducers/account.reducer.js
@@ -0,0 +1,36 @@
+
+import { 
+    getAccUserFulfilled,
+    getAccUserPending,
+    getAccUserRejected,
+    inc,
+    dec,
+    incByAmnt,
+    initUser,
+} from "../Actions";
+
+// Reducer
+
+// Initialized State With Some Value
+export function accountReducer(state={amount : 1}, action) {
+
+    switch(action.type) {
+        case inc : 
+            return {...state, amount : state.amount + 1}
+        case dec : 
+            return {...state, amount : state.amount - 1}
+        case incByAmnt :         
+            return {...state, amount : state.amount + action.payload}
+        case initUser :
+            return {...state, amount : action.payload}
+        case getAccUserFulfilled : 
+            return {...state, amount : action.payload, pending : false}
+        case getAccUserRejected : 
+            return {...state, error : action.error, pending : false}
+        case getAccUserPending : 
+            return {...state, pending : action.pending}
+        default : 
+            return state
+    }
+
+}
