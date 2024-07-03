78f7ee88bcad8e8de8eca9c6418117ea9d6a2b86
Rahul Rathor <www.rahulr41180@gmail.com>
2024-07-03 08:32:49 +0530

    System Architecture Code Update Done

---
+++ System Architecture/Course-1/1st_Introduction to System Architecture/1st_Definition/index.js
@@ -0,0 +1,9 @@
+
+// Definition :
+
+/* 
+System Architecture refers to the conceptual model that defines the structure, behavior, and more views of a system. 
+It serves as a blueprint for both the system and the project developing it. 
+This blueprint outlines the system's components or modules, their relationships, 
+and the principles and guidelines governing their design and evolution over time.
+*/
