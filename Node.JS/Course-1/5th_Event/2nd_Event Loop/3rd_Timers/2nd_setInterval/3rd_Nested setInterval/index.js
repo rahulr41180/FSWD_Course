48e1020e08b75137f0d552822b099026d04fddb5
Rahul Rathor <www.rahulr41180@gmail.com>
2024-07-17 21:19:29 +0530

    Node.JS Code Update Done

---
+++ Node.JS/Course-1/5th_Event/2nd_Event Loop/3rd_Timers/2nd_setInterval/3rd_Nested setInterval/index.js
@@ -0,0 +1,8 @@
+
+// Nested setInterval :
+
+/* 
+A nested setInterval() refers to using one setInterval() call inside another. This technique can be used to create more complex timing patterns.
+
+In a nested setInterval(), you can start another setInterval() within the callback function of an existing setInterval().
+*/
