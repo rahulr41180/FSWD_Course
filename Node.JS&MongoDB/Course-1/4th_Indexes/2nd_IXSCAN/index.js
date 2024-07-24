4e31bff789fb00baa78b6acc6a22a3696ac503d8
Rahul Rathor <www.rahulr41180@gmail.com>
2024-07-24 08:49:21 +0530

    MongoDB Code Update Done

---
+++ Node.JS&MongoDB/Course-1/4th_Indexes/2nd_IXSCAN/index.js
@@ -0,0 +1,12 @@
+
+// IXSCAN :
+
+/* 
+If we did indexing for collection property and then if we will do query for that indexed propert then MongoDB will do index scaning 
+for that perticular collection property.
+
+Index scaning is faster then collection secaning.
+Index scaning is following the binary search : Means whenever we did indexing for collection property then MongoDB create a seperate collection and 
+there MongoDB store all the different value of that collection property in an array in sort manner and it keep the collection property as property 
+of that new seperate collection and also it store the reference of that document in that new seperate collection.
+*/
