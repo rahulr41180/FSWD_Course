f264d7a04c1737c76bbc8da32fb8d605400c86a1
Rahul Rathor <97429450+rahulr41180@users.noreply.github.com>
2025-03-28 18:30:18 +0530

    update

---
+++ JavaScript/18th_OOPs/1st_Polymorphism/1st_Intro/1st_Example/index.ts
@@ -0,0 +1,13 @@
+
+/* 
+Polymorphism is one of the core principles of Object-Oriented Programming (OOP) that allows objects of different classes to be treated as objects of a common superclass. 
+It enables a single interface to be used for different data types, leading to code reusability and scalability.
+
+In JavaScript, polymorphism can be achieved through:
+
+Method Overriding (Runtime Polymorphism)
+
+Method Overloading (JavaScript does not support it directly, but it can be simulated)
+
+Duck Typing (Objects with similar behavior can be used interchangeably)
+*/
