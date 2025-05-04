e4ef333db3ef6efced09f0c74e897b851bbc4354
Rahul Rathor <97429450+rahulr41180@users.noreply.github.com>
2025-05-05 00:53:56 +0530

    update

--- Node.JS/10th_Worker Thread/1st_Project/src/server.ts
+++ Node.JS/10th_Worker Thread/1st_Project/src/server.ts
@@ -1,55 +1,30 @@
-
+// src/server.js
 import express from "express";
-import { Worker } from "worker_threads"
+import { Worker } from "worker_threads";
+import dotenv from "dotenv";
 import path from "path";
 
 const app = express();
+dotenv.config();
 const PORT = 3000;
 
-const worker = new Worker(path.resolve(__dirname, "worker.js"));
+const isDev = process.env.NODE_ENV !== "production";
 
-/* 
-Note: Workers can’t directly run .ts	They need compiled .js files.
 
-Do you need to build worker.ts every time?
-Yes, if you're editing worker.ts, you need to build it so the updated worker.js is available for worker_threads to run.
-
-That's because:
-
-worker_threads only executes compiled JavaScript files (.js)
-
-So it can't directly run .ts files unless you use hacks like ts-node, which are not optimal for performance
-*/
-
-/*
-This line creates a new Worker thread using the file worker.js.
-The file path points to the worker script which will run in its own thread.
-
-Node.js spawns a separate thread, executing the code in worker.js independently from the main thread.
-*/
+const pathToWorker = path.resolve(__dirname, "worker.ts").replace(/\\/g, '/');
+const worker = isDev ? new Worker(`
+    require('ts-node/register');
+    require('${pathToWorker}')    
+`, { eval: true }) : new Worker(path.resolve(__dirname, "worker.js"));
 
 // Listening / watching worker
 worker.on('message', (data) => {
     console.log('Message from Worker:', data);
 });
 
-/* 
-This sets up an event listener on the worker.
-
-It listens for the 'message' event, which the worker will emit when it sends data back.
-
-In this case, when the worker sends any message using parentPort.postMessage(...), this callback logs it.
-*/
-
 worker.on('error', (error) => {
     console.error('Worker Error:', error);
 });
-
-/*
-This listens for any runtime error inside the worker thread.
-
-If any unhandled exception happens inside worker.js, the error will be caught here and logged.
-*/
 
 worker.on('exit', (exitCode) => {
     if (exitCode !== 0) {
@@ -57,23 +32,9 @@ worker.on('exit', (exitCode) => {
     }
 });
 
-/* 
-This is triggered when the worker thread terminates (either normally or due to an error).
-
-exitCode of 0 means a normal exit.
-
-Any non-zero code indicates an issue, and we log it as an error.
-*/
-
 // emmiting worker
 // Sending data to the Worker
 worker.postMessage('Hello, Worker!');
-
-/*
-The main thread creates a worker and communicates with it via postMessage and listens for messages with .on('message').
-This line sends a message from the main thread to the worker.
-The message 'Hello, Worker!' is passed to the worker thread, where it will be received using an event listener on parentPort.
-*/
 
 app.get("/", (req, res) => {
     res.send("Hello from Express + TypeScript!");
--- Node.JS/10th_Worker Thread/1st_Project/src/worker.ts
+++ Node.JS/10th_Worker Thread/1st_Project/src/worker.ts
@@ -1,22 +1,9 @@
 
-// worker.js (Worker thread)
+// src/worker.js (Worker thread)
 import { parentPort } from "worker_threads";
-
-/* 
-This line gets access to parentPort, which is the communication channel between this worker and the main thread.
-Think of it like a walkie-talkie: parentPort is how the worker talks to the main thread.
-*/
 
 // Receive message from the main thread
 parentPort?.on('message', (msg) => {
     console.log('Message from Main:', msg);
     parentPort?.postMessage('Hello from Worker');
 });
-
-/*
-This listens for messages sent from the main thread via worker.postMessage(...).
-The worker listens for messages from the main thread and sends a response back.
-
-After receiving a message from the main thread, this sends a message back to the main thread.
-That’s where the worker.on('message') in server.ts picks it up.
-*/
